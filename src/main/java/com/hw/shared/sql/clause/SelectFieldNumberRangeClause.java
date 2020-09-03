6b0f7f7049b9840a732388597cc866d71e5889ab
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-09-02 22:53:29 -0400

    fix number range clause

--- src/main/java/com/hw/shared/sql/clause/SelectFieldNumberRangeClause.java
+++ src/main/java/com/hw/shared/sql/clause/SelectFieldNumberRangeClause.java
@@ -18,17 +18,17 @@ public class SelectFieldNumberRangeClause<T> extends WhereClause<T> {
         String[] split = query.split("\\$");
         List<Predicate> results = new ArrayList<>();
         for (String str : split) {
-            if (str.contains("<:")) {
-                int i = Integer.parseInt(query.replace("<:", ""));
+            if (str.contains("<=")) {
+                int i = Integer.parseInt(str.replace("<=", ""));
                 results.add(cb.lessThanOrEqualTo(root.get(entityFieldName), i));
-            } else if (str.contains(">:")) {
-                int i = Integer.parseInt(query.replace(">:", ""));
+            } else if (str.contains(">=")) {
+                int i = Integer.parseInt(str.replace(">=", ""));
                 results.add(cb.greaterThanOrEqualTo(root.get(entityFieldName), i));
             } else if (str.contains("<")) {
-                int i = Integer.parseInt(query.replace("<", ""));
+                int i = Integer.parseInt(str.replace("<", ""));
                 results.add(cb.lessThan(root.get(entityFieldName), i));
             } else if (str.contains(">")) {
-                int i = Integer.parseInt(query.replace(">", ""));
+                int i = Integer.parseInt(str.replace(">", ""));
                 results.add(cb.greaterThan(root.get(entityFieldName), i));
             } else {
                 throw new UnsupportedQueryException();

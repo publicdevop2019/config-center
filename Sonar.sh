363422ab6cac362290ff8a05ade41590db873913
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-09-13 23:40:04 -0400

    after scan

--- Sonar.sh
+++ Sonar.sh
@@ -1,16 +1,16 @@
 declare -A configPathMap
-#configPathMap["mt0-oauth2"]="mt0-oauth2"
+configPathMap["mt0-oauth2"]="mt0-oauth2"
 #configPathMap["mt1-proxy"]="mt1-proxy"
 #configPathMap["mt2-user-profile"]="mt2-user-profile"
-configPathMap["mt3-product"]="mt3-product"
+#configPathMap["mt3-product"]="mt3-product"
 #configPathMap["mt4-messenger"]="mt4-messenger"
 #configPathMap["mt5-file-upload"]="mt5-file-upload"
 #configPathMap["mt6-payment"]="mt6-payment"
 #configPathMap["mt13-bbs"]="mt13-bbs"
 
 declare -A configKeyMap
-configKeyMap["mt0-oauth2"]="com.hw:oauth2"
-configKeyMap["mt1-proxy"]="com.hw:proxy"
+configKeyMap["mt0-oauth2"]="publicdevop2019_mt0-oauth2"
+configKeyMap["mt1-proxy"]="publicdevop2019_mt1-proxy"
 configKeyMap["mt2-user-profile"]="publicdevop2019_mt2-user-profile"
 configKeyMap["mt3-product"]="publicdevop2019_mt3-product"
 configKeyMap["mt4-messenger"]="com.hw:messenger"
